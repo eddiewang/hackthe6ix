6e1742498d2b24812f43e0920007d21c7d67c221
lisuwen <li.suwen@hotmail.com>
2016-08-20 20:37:20 -0400

    added emojis

--- app/components/Post/index.js
+++ app/components/Post/index.js
@@ -9,6 +9,12 @@ import React from 'react';
 import styles from './styles.css';
 import Text from 'components/Text';
 
+import JoyEmoji from 'static/images/joy_emoji.svg';
+import AngerEmoji from 'static/images/anger_emoji.svg';
+import FearEmoji from 'static/images/fear_emoji.svg';
+import SadnessEmoji from 'static/images/sadness_emoji.svg';
+import SurpriseEmoji from 'static/images/surprise_emoji.svg';
+
 class Post extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
@@ -20,6 +26,29 @@ class Post extends React.Component { // eslint-disable-line react/prefer-statele
           </div>
           <Text type="post">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cupiditate quo ad minus aspernatur suscipit similique eos consequatur asperiores corporis, perferendis impedit expedita, placeat, doloremque, omnis fuga officia sed excepturi?</Text>
           <Text type="post">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cupiditate quo ad minus aspernatur suscipit similique eos consequatur asperiores corporis, perferendis impedit expedita, placeat, doloremque, omnis fuga officia sed excepturi?</Text>
+        </div>
+        <div className={styles.emojiContainer}>
+          <ul>
+            <li>
+
+            // <svg width="200" height="200">
+            //   <linearGradient id="lg" x1="0.5" y1="1" x2="0.5" y2="0">
+            //       <stop offset="0%" stop-opacity="1" stop-color="blue"/>
+            //       <stop offset="40%" stop-opacity="1" stop-color="blue"/>
+            //       <stop offset="40%" stop-opacity="0" stop-color="blue"/>
+            //       <stop offset="100%" stop-opacity="0" stop-color="blue"/>
+            //   </linearGradient>
+
+            // <circle cx="50" cy="50" r="45" fill="url(#lg)" stroke="crimson" stroke-width="5"/>
+            // </svg>
+
+              <img src={JoyEmoji}/>
+            </li>
+            <li><img src={AngerEmoji}/></li>
+            <li><img src={FearEmoji}/></li>
+            <li><img src={SadnessEmoji}/></li>
+            <li><img src={SurpriseEmoji}/></li>
+          </ul>
         </div>
       </div>
     );
